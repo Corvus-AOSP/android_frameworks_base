1ae8aa47e6b8745263cbbd0823e7954398d03a43
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Add api to take screenshots
    abc: extracted from DUI
    aex: Add delay
    
    Change-Id: I2ac0bf9cf6641a475ea9a3c9b714550bc55ee13b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/com/android/internal/util/corvus/CorvusUtils.java
+++ core/java/com/android/internal/util/corvus/CorvusUtils.java
@@ -17,11 +17,17 @@
 package com.android.internal.util.corvus;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
+import android.view.InputDevice;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -29,6 +35,9 @@ import com.android.internal.statusbar.IStatusBarService;
  * Some custom utilities
  */
 public class CorvusUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     public static void switchScreenOff(Context ctx) {
         PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
@@ -78,4 +87,17 @@ public class CorvusUtils {
         }, 20);
     }
 
+    public static void takeScreenshot(boolean full) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ie) {
+            // Do nothing
+        }
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 }
