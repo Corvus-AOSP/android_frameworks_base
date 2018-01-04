5393ec36368ef2f6b37848b934c0f5ea6a5e2d3b
ezio84 <brabus84@gmail.com>
2018-01-04 10:26:17 +0100

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    @saikiran2001: Adapt to android 11
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Change-Id: Id90aae1b4626fe587be7d29171c82d6416ffd95b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4929,6 +4929,14 @@ public final class Settings {
         public static final String OMNI_QS_LAYOUT_ROWS = "qs_layout_rows";
 
         /**
+         * Wheter to play notification sound and vibration if screen is ON
+         * 0 - never
+         * 1 - always
+         * @hide
+         */
+        public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
+
+        /**
          * Whether to show the battery info on the lockscreen while charging
          * @hide
          */
