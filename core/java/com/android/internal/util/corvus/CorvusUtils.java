fc65090b95730f948ca299adf2c505817d548d29
PMS22 <prathams99@rediff.com>
2018-10-16 02:25:57 +0530

    SystemUI: CompassTile: Add a device supports compass check
    - Previously it showed up on devices even without compass support
      making it a bloat
    
    Change-Id: Iba3b29e4be2c0fd5f97573c2d0abbde7ad1db995
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/com/android/internal/util/corvus/CorvusUtils.java
+++ core/java/com/android/internal/util/corvus/CorvusUtils.java
@@ -19,6 +19,11 @@ package com.android.internal.util.corvus;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.input.InputManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -100,4 +105,11 @@ public class CorvusUtils {
             e.printStackTrace();
         }
     }
+
+    public static boolean deviceHasCompass(Context ctx) {
+        SensorManager sm = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
+        return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
+                && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
+    }
+
 }
