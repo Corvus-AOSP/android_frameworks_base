ffcc05acc24c33c9636584f2a29297f29527503e
binzhang <binzhang@codeaurora.org>
2018-11-22 11:12:45 +0800

    add VSYNC scheduled flag and avoid two doFrame calls in one period
    This flag can make sure touch response optimizaion only
    takes effect when there is no Vsync requesut from previous
    frame. It will not block animation.
    It can also make sure there will be only one doFrame call
    in one Vsync period.
    
    Change-Id: I5ce0ab7ba87ba8a88dbba942a28137be91928638
    CRs-Fixed: 2346476
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/view/Choreographer.java
+++ core/java/android/view/Choreographer.java
@@ -181,6 +181,7 @@ public final class Choreographer {
     private long mFrameIntervalNanos;
     private boolean mDebugPrintNextFrameTimeDelta;
     private int mFPSDivisor = 1;
+    private boolean mIsVsyncScheduled = false;
 
     /**
      * Contains information about the current frame for jank-tracking,
@@ -626,6 +627,47 @@ public final class Choreographer {
     private void scheduleFrameLocked(long now) {
         if (!mFrameScheduled) {
             mFrameScheduled = true;
+            if (OPTS_INPUT) {
+                if ((!mIsVsyncScheduled) &&
+                    ((System.nanoTime() - mLastFrameTimeNanos) > mFrameIntervalNanos)) {
+                    Trace.traceBegin(Trace.TRACE_TAG_VIEW, "scheduleFrameLocked-mMotionEventType:"
+                                     + mMotionEventType + " mTouchMoveNum:" + mTouchMoveNum
+                                     + " mConsumedDown:" + mConsumedDown + " mConsumedMove:"
+                                     + mConsumedMove);
+                    Trace.traceEnd(Trace.TRACE_TAG_VIEW);
+                    synchronized(this) {
+                        switch(mMotionEventType) {
+                            case MOTION_EVENT_ACTION_DOWN:
+                                mConsumedMove = false;
+                                if (!mConsumedDown) {
+                                    Message msg = mHandler.obtainMessage(MSG_DO_FRAME);
+                                    msg.setAsynchronous(true);
+                                    mHandler.sendMessageAtFrontOfQueue(msg);
+                                    mConsumedDown = true;
+                                    return;
+                                }
+                                break;
+                            case MOTION_EVENT_ACTION_MOVE:
+                                mConsumedDown = false;
+                                if ((mTouchMoveNum == 1) && !mConsumedMove) {
+                                    Message msg = mHandler.obtainMessage(MSG_DO_FRAME);
+                                    msg.setAsynchronous(true);
+                                    mHandler.sendMessageAtFrontOfQueue(msg);
+                                    mConsumedMove = true;
+                                    return;
+                                }
+                                break;
+                            case MOTION_EVENT_ACTION_UP:
+                            case MOTION_EVENT_ACTION_CANCEL:
+                                mConsumedMove = false;
+                                mConsumedDown = false;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
             if (USE_VSYNC) {
                 if (DEBUG_FRAMES) {
                     Log.d(TAG, "Scheduling next frame on vsync.");
@@ -664,6 +706,7 @@ public final class Choreographer {
     void doFrame(long frameTimeNanos, int frame) {
         final long startNanos;
         synchronized (mLock) {
+            mIsVsyncScheduled = false;
             if (!mFrameScheduled) {
                 return; // no work to do
             }
@@ -830,6 +873,7 @@ public final class Choreographer {
     @UnsupportedAppUsage
     private void scheduleVsyncLocked() {
         mDisplayEventReceiver.scheduleVsync();
+        mIsVsyncScheduled = true;
     }
 
     private boolean isRunningOnLooperThreadLocked() {
