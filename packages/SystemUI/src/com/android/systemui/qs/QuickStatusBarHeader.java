67189e46d032252781693f87050f09053c4c5132
Pranav Vashi <neobuddy89@gmail.com>
2019-02-23 04:20:47 +0530

    Add separate tunable for QS clock
    * Add separate switch to disable QS clock.
    * Add mQsHeader boolean to identify QS clock. Credits: ezio84.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I9dfaf11f841407f5cd03fa9bae0d60d2f6db73d4

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -67,11 +67,8 @@ import java.util.List;
 public class QuickStatusBarHeader extends FrameLayout implements
         View.OnClickListener, View.OnLongClickListener, TunerService.Tunable {
 
-    private static final int CLOCK_POSITION_LEFT = 2;
-    private static final int CLOCK_POSITION_HIDE = 3;
-
-    private static final String STATUS_BAR_CLOCK =
-            "system:" + Settings.System.STATUS_BAR_CLOCK;
+    private static final String SHOW_QS_CLOCK =
+            "system:" + Settings.System.SHOW_QS_CLOCK;
 
     private boolean mExpanded;
     private boolean mQsDisabled;
@@ -169,6 +166,7 @@ public class QuickStatusBarHeader extends FrameLayout implements
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
         mClockView.setOnLongClickListener(this);
+        mClockView.setQsHeader();
         mDatePrivacySeparator = findViewById(R.id.space);
         // Tint for the battery icons are handled in setupHost()
         mBatteryRemainingIcon = findViewById(R.id.batteryRemainingIcon);
@@ -188,7 +186,7 @@ public class QuickStatusBarHeader extends FrameLayout implements
                 .build();
 
         Dependency.get(TunerService.class).addTunable(this,
-                STATUS_BAR_CLOCK);
+                SHOW_QS_CLOCK);
     }
 
     void onAttach(TintedIconManager iconManager,
@@ -642,10 +640,10 @@ public class QuickStatusBarHeader extends FrameLayout implements
     @Override
     public void onTuningChanged(String key, String newValue) {
         switch (key) {
-            case STATUS_BAR_CLOCK:
-                int showClock =
-                        TunerService.parseInteger(newValue, CLOCK_POSITION_LEFT);
-                mClockView.setClockVisibleByUser(showClock != CLOCK_POSITION_HIDE);
+            case SHOW_QS_CLOCK:
+                boolean showClock =
+                        TunerService.parseIntegerSwitch(newValue, true);
+                mClockView.setClockVisibleByUser(showClock);
                 break;
             default:
                 break;
