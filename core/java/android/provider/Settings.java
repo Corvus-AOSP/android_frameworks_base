bb6ea1db459005d06f4790c1338af647c1d33268
Nico60 <ndhouailly@gmail.com>
2019-03-14 07:04:00 -0400

    TrafficSB: Hide arrows in statusbar traffic indicators [1/2]
    arrow-edits:
    adapt code to our dynamic arrows method
    
    Change-Id: Ie50dbe3f54495d4b5c0c0a51969df595de7a1386
    Signed-off-by: Sebita <kjjjnob.seba@gmail.com>
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5067,6 +5067,12 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
         /**
+         * Whether to disable showing arrows in statusbar network traffic indicators
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
