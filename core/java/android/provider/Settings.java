a604183f4086689b10cab32c4a2492a950d2f6cb
ezio84 <brabus84@gmail.com>
2019-11-02 10:23:25 +0100

    Allow to choose lockscreen or Ambient on wake gestures [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    PS: we could merge the "else if (isWakeLockScreen)" case in the
    longpress/wakegestures one considering it also triggers
    a requestPulse, but let's keep it as it's now just to avoid
    future merge conflicts there
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5081,6 +5081,14 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
 
+        
+        /**
+         * Whether to show ambient or lockscreen if AoD is disabled
+         * and we do a wake gesture like lift to wake or double tap
+         *
+         * @hide
+         */
+        public static final String AMBIENT_WAKE_GESTURES = "ambient_wake_gestures";
 
          /**
          * media artwork wallpaper blur level on lockscreen
