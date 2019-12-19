0067fa1125932c730d1d32d8deca1981837f4c5c
Ido Ben-Hur <idoybh2@gmail.com>
2019-12-19 15:27:43 +0200

    SystemUI: Allow changing duration and repeats count of ambient edge light [1/2]
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ib2dcdfad3dc0e69155b72a5be0228550a61e7607
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5743,6 +5743,15 @@ public final class Settings {
          */
         public static final String NOTIFICATION_PULSE_COLOR_AUTOMATIC = "ambient_notification_light_color_automatic";
 
+        /**
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_DURATION = "ambient_light_duration";
+
+        /**
+         * @hide
+         */
+        public static final String AMBIENT_LIGHT_REPEAT_COUNT = "ambient_light_repeat_count";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
