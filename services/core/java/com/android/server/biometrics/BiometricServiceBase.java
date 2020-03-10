3561edbcd425e74bc583778763702593e5de33b5
Pierre-Hugues Husson <phh@phh.me>
2020-03-10 23:30:17 +0100

    Allow disabling of fingerprint cleanups, needed on some Realme devices that cant enumerate
    Change-Id: I8a486e707712b81711fb1a691faec029499fa897

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -346,6 +346,7 @@ public abstract class BiometricServiceBase extends SystemService
         private List<? extends BiometricAuthenticator.Identifier> mEnrolledList;
         // List of templates to remove from the HAL
         private List<BiometricAuthenticator.Identifier> mUnknownHALTemplates = new ArrayList<>();
+        final boolean mNocleanup = android.os.SystemProperties.getBoolean("persist.sys.phh.fingerprint.nocleanup", false);
 
         InternalEnumerateClient(Context context,
                 DaemonWrapper daemon, long halDeviceId, IBinder token,
@@ -391,8 +392,10 @@ public abstract class BiometricServiceBase extends SystemService
                 Slog.e(getTag(), "doTemplateCleanup(): Removing dangling template from framework: "
                         + identifier.getBiometricId() + " "
                         + identifier.getName());
-                mUtils.removeBiometricForUser(getContext(),
-                        getTargetUserId(), identifier.getBiometricId());
+                if(!mNocleanup) {
+                    mUtils.removeBiometricForUser(getContext(),
+                            getTargetUserId(), identifier.getBiometricId());
+                }
                 FrameworkStatsLog.write(FrameworkStatsLog.BIOMETRIC_SYSTEM_HEALTH_ISSUE_DETECTED,
                         statsModality(),
                         BiometricsProtoEnums.ISSUE_UNKNOWN_TEMPLATE_ENROLLED_FRAMEWORK);
