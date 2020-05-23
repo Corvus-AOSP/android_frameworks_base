a4968a4efd7de0d5ac4d677a38016e5601a3f0dc
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-23 09:14:37 +0530

    Utils: add method to get today's passed time
    Signed-off-by: ashwatthama <sai404142@gmail.com>
    Change-Id: Idef366c8f0c9729655057b2b134cf2a95b8a510e

--- core/java/com/android/internal/util/corvus/Utils.java
+++ core/java/com/android/internal/util/corvus/Utils.java
@@ -44,6 +44,7 @@ import android.os.SystemClock;
 import android.net.NetworkInfo;
 import android.net.ConnectivityManager;
 import android.os.SystemProperties;
+import android.text.format.Time;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -70,6 +71,15 @@ public class Utils {
 
         NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return wifi.isConnected();
+    }
+
+	// Returns today's passed time in Millisecond
+    public static long getTodayMillis() {
+        final long passedMillis;
+        Time time = new Time();
+        time.set(System.currentTimeMillis());
+        passedMillis = ((time.hour * 60 * 60) + (time.minute * 60) + time.second) * 1000;
+        return passedMillis;
     }
 
 	// Check if device is connected to the internet
