94d5bc3ac22da98c0842e92eaeba11e2b9daf03f
Pranav Vashi <neobuddy89@gmail.com>
2020-07-19 04:24:41 +0530

    base: notch-city: Dejank immersive mode with QS panel
    * Adapted for Android 11
    * Based on https://github.com/crdroidandroid/android_frameworks_base/commit/4573b2f6846d49e7859e18e720cc751bc49347b6
    
    Change-Id: I20ef5c2f8013d9d92a89d2cea8c2b11fc162d1f2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -88,6 +88,7 @@ import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.classifier.Classifier;
 import com.android.systemui.RetickerAnimations;
+import com.android.systemui.ScreenDecorations;
 import com.android.systemui.dagger.qualifiers.DisplayId;
 import com.android.systemui.doze.DozeLog;
 import com.android.systemui.evolution.NotificationLightsView;
@@ -451,6 +452,8 @@ public class NotificationPanelViewController extends PanelViewController {
     private GestureDetector mLockscreenDoubleTapToSleep;
     private boolean mIsLockscreenDoubleTapEnabled;
 
+    private int mStatusBarHeight;
+
     /**
      * Cache the resource id of the theme to avoid unnecessary work in onThemeChanged.
      *
@@ -485,6 +488,7 @@ public class NotificationPanelViewController extends PanelViewController {
      * the keyguard is dismissed to show the status bar.
      */
     private boolean mDelayShowingKeyguardStatusBar;
+    private ScreenDecorations mScreenDecorations;
 
     private NotificationLightsView mPulseLightsView;
     private boolean mPulseLightHandled;
@@ -643,6 +647,8 @@ public class NotificationPanelViewController extends PanelViewController {
             mView.getOverlay().add(new DebugDrawable());
         }
 
+        mScreenDecorations = Dependency.get(ScreenDecorations.class);
+
         onFinishInflate();
     }
 
@@ -718,6 +724,8 @@ public class NotificationPanelViewController extends PanelViewController {
                 R.dimen.keyguard_indication_bottom_padding);
         mQsNotificationTopPadding = mResources.getDimensionPixelSize(
                 R.dimen.qs_notification_padding);
+        mStatusBarHeight = mResources.getDimensionPixelSize(
+                com.android.internal.R.dimen.status_bar_height_portrait);
         mShelfHeight = mResources.getDimensionPixelSize(R.dimen.notification_shelf_height);
         mDarkIconSize = mResources.getDimensionPixelSize(R.dimen.status_bar_icon_drawing_size_dark);
         int statusbarHeight = mResources.getDimensionPixelSize(
@@ -1798,6 +1806,13 @@ public class NotificationPanelViewController extends PanelViewController {
         } else if (height <= mQsMinExpansionHeight && mQsExpanded) {
             setQsExpanded(false);
         }
+        if (mKeyguardShowing) {
+            if (height > mStatusBarHeight) {
+                setTopCorners(false);
+            } else {
+                setTopCorners(true);
+            }
+        }
         mQsExpansionHeight = height;
         updateQsExpansion();
         requestScrollerTopPaddingUpdate(false /* animate */);
@@ -2144,6 +2159,13 @@ public class NotificationPanelViewController extends PanelViewController {
                     mQsMinExpansionHeight + t * (mQsMaxExpansionHeight - mQsMinExpansionHeight);
             setQsExpansion(targetHeight);
         }
+        if (!mKeyguardShowing) {
+            if (expandedHeight > mStatusBarHeight) {
+                setTopCorners(false);
+            } else {
+                setTopCorners(true);
+            }
+        }
         updateExpandedHeight(expandedHeight);
         updateHeader();
         updateNotificationTranslucency();
@@ -2364,6 +2386,12 @@ public class NotificationPanelViewController extends PanelViewController {
                 getExpandedFraction());
         float alpha = Math.min(expansionAlpha, 1 - getQsExpansionFraction());
         mBigClockContainer.setAlpha(alpha);
+    }
+
+    private void setTopCorners(boolean enable) {
+        if (mScreenDecorations == null)
+            mScreenDecorations = Dependency.get(ScreenDecorations.class);
+    	mScreenDecorations.setTopCorners(enable);
     }
 
     @Override
