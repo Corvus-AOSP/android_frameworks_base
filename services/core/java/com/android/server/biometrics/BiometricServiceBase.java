c463626d985cdf6d6985672ee3b60d2b0a5289c1
kaluoshi <kaluoshi@users.noreply.github.com>
2020-09-25 15:03:49 +0000

    fingerprint: notify client when cancelling succeeded
    * Some fingerprint hardware hals might not notify userspace in time
      in some scenarios when successful authentication cancel is performed.
    * This is observed for example in some devices that have power and/or home
      buttons merged with fingerprint sensor, but not limited to such cases.
    * Add a opt-in configuration to enable client authentication cancel from
      fingerprint service when successful.
    * This addresses originally an issue where requesting fingerprint to be
      disabled and then enabled will wait till cancelling timeout before changing
      fingerprint sensor state and start authenticating again.
    
    Change-Id: I6a6795fbb795f0c6a4ff8ad27ac807e2f744c2d9
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -142,6 +142,8 @@ public abstract class BiometricServiceBase extends SystemService
         public int permanentLockout; // total number of permanent lockouts
     }
 
+    private final boolean mNotifyClient;
+
     /**
      * @return the log tag.
      */
@@ -665,6 +667,10 @@ public abstract class BiometricServiceBase extends SystemService
         mMetricsLogger = new MetricsLogger();
         mPostResetRunnableForAllClients = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_fingerprintPostResetRunnableForAllClients);
+        mCleanupUnusedFingerprints = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_cleanupUnusedFingerprints);
+        mNotifyClient = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_notifyClientOnFingerprintCancelSuccess);
     }
 
     @Override
@@ -932,7 +938,11 @@ public abstract class BiometricServiceBase extends SystemService
                             + ", fromClient: " + fromClient);
                     // If cancel was from BiometricService, it means the dialog was dismissed
                     // and authentication should be canceled.
-                    client.stop(client.getToken() == token);
+                    final int stopResult = client.stop(client.getToken() == token);
+                    if (mNotifyClient && (stopResult == 0)) {
+                        handleError(mHalDeviceId,
+                                BiometricConstants.BIOMETRIC_ERROR_CANCELED, 0);
+                    }
                 } else {
                     if (DEBUG) Slog.v(getTag(), "Can't stop client " + client.getOwnerString()
                             + " since tokens don't match. fromClient: " + fromClient);
