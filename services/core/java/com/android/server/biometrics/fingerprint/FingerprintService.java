9ce19655bbbb2b8e7220a38919ad94a8f0d43da4
Joey Huab <joey@evolution-x.org>
2020-10-18 05:02:42 +0900

    Use FodUtils for FOD config checking
    Change-Id: Iba764286d9e5ecd3f4b786e2070ede1b1af1e9c0

--- services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/biometrics/fingerprint/FingerprintService.java
@@ -66,6 +66,7 @@ import com.android.internal.annotations.GuardedBy;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.util.DumpUtils;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.util.corvus.fod.FodUtils;
 import com.android.server.SystemServerInitThreadPool;
 import com.android.server.biometrics.AuthenticationClient;
 import com.android.server.biometrics.BiometricServiceBase;
@@ -79,8 +80,6 @@ import com.android.server.biometrics.Utils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import lineageos.app.LineageContextConstants;
 
 import vendor.lineage.biometrics.fingerprint.inscreen.V1_0.IFingerprintInscreen;
 
@@ -892,9 +891,8 @@ public class FingerprintService extends BiometricServiceBase {
         mAlarmManager = context.getSystemService(AlarmManager.class);
         context.registerReceiver(mLockoutReceiver, new IntentFilter(getLockoutResetIntent()),
                 getLockoutBroadcastPermission(), null /* handler */);
-        PackageManager packageManager = context.getPackageManager();
         mLockPatternUtils = new LockPatternUtils(context);
-        mHasFod = packageManager.hasSystemFeature(LineageContextConstants.Features.FOD);
+        mHasFod = FodUtils.hasFodSupport(context);
     }
 
     @Override
