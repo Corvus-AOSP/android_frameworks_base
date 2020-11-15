1fb7940fc6a75b992b48eb1cc95ea7a3724ee176
Genkzsz11 <Genkzsz11@gmail.com>
2020-11-15 07:39:08 +0100

    base:update font list!
    Change-Id: Ibe9326363fc5a8c3c8680d11acb979ca26ca226f

--- core/java/com/android/internal/util/corvus/ThemesUtils.java
+++ core/java/com/android/internal/util/corvus/ThemesUtils.java
@@ -72,25 +72,24 @@ public class ThemesUtils {
 
 
     public static final String[] FONTS = {
+            "com.android.theme.font.notoserifsource",
             "com.android.theme.font.aclonicasource",
             "com.android.theme.font.amarantesource",
+            "com.android.theme.font.arvolato",
             "com.android.theme.font.bariolsource",
-            "com.android.theme.font.burnstownsource",
             "com.android.theme.font.cagliostrosource",
+            "com.android.theme.font.circularstd",
             "com.android.theme.font.comicsanssource",
             "com.android.theme.font.coolstorysource",
             "com.android.theme.font.firasans",
             "com.android.theme.font.googlesans",
             "com.android.theme.font.lgsmartgothicsource",
-            "com.android.theme.font.notoserifsource",
-            "com.android.theme.font.slateforoneplus",
             "com.android.theme.font.rosemarysource",
+            "com.android.theme.font.rubikrubik",
             "com.android.theme.font.samsungone",
+            "com.android.theme.font.sfpro",
+            "com.android.theme.font.slateforoneplus",
             "com.android.theme.font.sonysketchsource",
             "com.android.theme.font.surfersource",
-            "com.android.theme.font.sfpro",
-            "com.android.theme.font.circularstd",
-            "com.android.theme.font.rubikrubik",
-            "com.android.theme.font.arvolato",
     };
 }
