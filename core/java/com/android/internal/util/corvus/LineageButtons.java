4e91429b19d87bed197795a2e40363791bd9507a
ezio84 <brabus84@gmail.com>
2020-11-21 15:56:14 +0530

    ScreenOFF volume skiptrack (with remote Spotify support)
    and other players that allow music controls for remote devices (PC, PS4)
    through the media notification
    
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- core/java/com/android/internal/util/corvus/LineageButtons.java
+++ core/java/com/android/internal/util/corvus/LineageButtons.java
@@ -21,28 +21,32 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.media.AudioManager;
+import android.media.session.MediaController;
 import android.media.session.MediaSessionLegacyHelper;
+import android.media.session.MediaSessionManager;
+import android.media.session.PlaybackState;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.Settings;
-import android.util.Slog;
 import android.view.KeyEvent;
 import android.view.ViewConfiguration;
 
+import java.util.List;
+
 public final class LineageButtons {
     private final String TAG = "LineageButtons";
-    private final boolean DEBUG = false;
 
     private static final int MSG_DISPATCH_VOLKEY_WITH_WAKELOCK = 1;
 
     private final Context mContext;
-    private ButtonHandler mHandler;
+    private final ButtonHandler mHandler;
 
     private boolean mIsLongPress = false;
 
     private boolean mVolBtnMusicControls = false;
+    private final MediaSessionManager mMediaSessionManager;
 
     private class ButtonHandler extends Handler {
         @Override
@@ -51,11 +55,7 @@ public final class LineageButtons {
                 case MSG_DISPATCH_VOLKEY_WITH_WAKELOCK:
                     KeyEvent ev = (KeyEvent) msg.obj;
                     mIsLongPress = true;
-                    if (DEBUG) {
-                        Slog.d(TAG, "Dispatching key to audio service");
-                    }
-                    dispatchMediaKeyToAudioService(ev);
-                    dispatchMediaKeyToAudioService(KeyEvent.changeAction(ev, KeyEvent.ACTION_UP));
+                    onSkipTrackEvent(ev);
                     break;
             }
         }
@@ -76,6 +76,7 @@ public final class LineageButtons {
     public LineageButtons(Context context) {
         mContext = context;
         mHandler = new ButtonHandler();
+        mMediaSessionManager = mContext.getSystemService(MediaSessionManager.class);
 
         SettingsObserver observer = new SettingsObserver(new Handler());
         observer.observe();
@@ -83,23 +84,12 @@ public final class LineageButtons {
         sInstance = this;
     }
 
-     public void skipTrack() {
-        long when = SystemClock.uptimeMillis();
-        KeyEvent newEvent = new KeyEvent(when, when,
-                KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_NEXT, 0);
-        onSkipTrackEvent(newEvent);
-    }
-
-
     public boolean handleVolumeKey(KeyEvent event, boolean isInteractive) {
         final boolean down = event.getAction() == KeyEvent.ACTION_DOWN;
         final int keyCode = event.getKeyCode();
 
         if (isInteractive) {
             // nothing to do here for now
-            if (DEBUG) {
-                Slog.d(TAG, "Skipping because interactive");
-            }
             return false;
         }
 
@@ -116,14 +106,8 @@ public final class LineageButtons {
                     int newKeyCode = (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN
                             ? KeyEvent.KEYCODE_MEDIA_PREVIOUS
                             : KeyEvent.KEYCODE_MEDIA_NEXT);
-
                     KeyEvent newEvent = new KeyEvent(event.getDownTime(), event.getEventTime(),
                             event.getAction(), newKeyCode, 0);
-                    if (DEBUG) {
-                        Slog.d(TAG, "Queueing media " +
-                                (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN ? "previous" : "next") +
-                                " event " + newEvent);
-                    }
                     Message msg = mHandler.obtainMessage(MSG_DISPATCH_VOLKEY_WITH_WAKELOCK,
                             newEvent);
                     msg.setAsynchronous(true);
@@ -151,11 +135,42 @@ public final class LineageButtons {
         return true;
     }
 
-    void dispatchMediaKeyToAudioService(KeyEvent ev) {
-        if (DEBUG) {
-            Slog.d(TAG, "Dispatching KeyEvent " + ev + " to audio service");
+    private void triggerKeyEvents(KeyEvent evDown, MediaController controller) {
+        final KeyEvent evUp = KeyEvent.changeAction(evDown, KeyEvent.ACTION_UP);
+        mHandler.post(() -> controller.dispatchMediaButtonEvent(evDown));
+        mHandler.postDelayed(() -> controller.dispatchMediaButtonEvent(evUp), 20);
+    }
+
+    public void onSkipTrackEvent(KeyEvent ev) {
+        if (mMediaSessionManager != null) {
+            final List<MediaController> sessions
+                    = mMediaSessionManager.getActiveSessionsForUser(
+                    null, UserHandle.USER_ALL);
+            for (MediaController aController : sessions) {
+                if (PlaybackState.STATE_PLAYING ==
+                        getMediaControllerPlaybackState(aController)) {
+                    triggerKeyEvents(ev, aController);
+                    break;
+                }
+            }
         }
-        MediaSessionLegacyHelper.getHelper(mContext).sendMediaButtonEvent(ev, true);
+    }
+
+    public void skipTrack() {
+        long when = SystemClock.uptimeMillis();
+        KeyEvent newEvent = new KeyEvent(when, when,
+                KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_NEXT, 0);
+        onSkipTrackEvent(newEvent);
+    }
+
+    private int getMediaControllerPlaybackState(MediaController controller) {
+        if (controller != null) {
+            final PlaybackState playbackState = controller.getPlaybackState();
+            if (playbackState != null) {
+                return playbackState.getState();
+            }
+        }
+        return PlaybackState.STATE_NONE;
     }
 
     class SettingsObserver extends ContentObserver {
@@ -166,9 +181,9 @@ public final class LineageButtons {
         void observe() {
             ContentResolver resolver = mContext.getContentResolver();
 
-            /*resolver.registerContentObserver(LineageSettings.System.getUriFor(
-                    LineageSettings.System.VOLBTN_MUSIC_CONTROLS),
-                            false, this, UserHandle.USER_ALL);*/
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.VOLUME_BUTTON_MUSIC_CONTROL),
+                            false, this, UserHandle.USER_ALL);
 
             update();
         }
@@ -180,15 +195,10 @@ public final class LineageButtons {
 
         private void update() {
             ContentResolver resolver = mContext.getContentResolver();
-            
 
-            mVolBtnMusicControls = /*LineageSettings.System.getIntForUser(
-                    resolver, LineageSettings.System.VOLBTN_MUSIC_CONTROLS, 1,
-                    UserHandle.USER_CURRENT) == 1;*/false;
-
-            if (DEBUG) {
-                Slog.d(TAG, "music controls enabled = " + mVolBtnMusicControls);
-            }
+            mVolBtnMusicControls = Settings.System.getIntForUser(
+                    resolver, Settings.System.VOLUME_BUTTON_MUSIC_CONTROL, 1,
+                    UserHandle.USER_CURRENT) == 1;
         }
     }
 }
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -92,6 +92,7 @@ public class SystemSettings {
         Settings.System.QS_PANEL_BG_USE_NEW_TINT,
         Settings.System.LOCKSCREEN_MEDIA_METADATA,
         Settings.System.LOCKSCREEN_MEDIA_ART,
+       
 
     };
 }
