fa1d7444bd6249aceaebef195a1f12c80fc48f6d
Anushek Prasal <anushekprasal@gmail.com>
2020-11-24 23:27:29 +0530

    base: Add toggle to disable charging animation [1/2]
    Change-Id: I8982623ce5d231d1a29dfa934179214a6745faae
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    
    Signed-off-by: Zaviyar786 <beingme639@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5111,6 +5111,13 @@ public final class Settings {
         public static final String LOCKSCREEN_MEDIA_BLUR = "lockscreen_media_blur";
 
         /**
+         * Whether to show charging animation
+         * @hide
+         */
+        public static final String CHARGING_ANIMATION = "charging_animation";
+        
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
