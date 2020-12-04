f16f919950ed0a6bc9e7d6f848c53496a97eaa3d
Ido Ben-Hur <idoybh2@gmail.com>
2020-12-04 08:48:43 +0000

    base: Allow showing bottom brightness slider above QS footer [1/2]
    Change-Id: I66840410a6ec1a4d358fd556b804823bc1f1f2f7
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -97,5 +97,6 @@ public class SystemSettings {
         Settings.System.QS_SHOW_BRIGHTNESS,
         Settings.System.QS_BRIGHTNESS_POSITION_BOTTOM,
         Settings.System.QS_SHOW_AUTO_BRIGHTNESS,
+        Settings.System.QS_SHOW_BRIGHTNESS_ABOVE_FOOTER,
     };
 }
