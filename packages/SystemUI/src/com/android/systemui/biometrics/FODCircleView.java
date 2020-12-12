8f328835156e4ac9cb78e4b1ad733fdecd7aca00
Ritzz <riteshm321@gmail.com>
2020-12-12 19:06:58 +0100

    Revert "FODCircleView: Hide fod while going to sleep"
    This reverts commit 34c50c67e9103415aef8c6284722912d1b4bcc6a.

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -128,7 +128,7 @@ public class FODCircleView extends ImageView {
         }
 
         @Override
-        public void onStartedGoingToSleep(int why) {
+        public void onScreenTurnedOff() {
             hide();
         }
 
