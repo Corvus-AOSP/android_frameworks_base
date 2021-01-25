c99b8adaf5ee34c21cbe4c00cc527f50e9bb742e
Ritzz <riteshm321@gmail.com>
2021-01-25 09:19:14 +0100

    Revert "Introduce QS tile style picker [1/3]"
    This reverts commit b86fe48bb817064cf42e70d4c586329d75c68461.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5326,11 +5326,6 @@ public final class Settings {
          */
         public static final String FORCE_SHOW_NAVBAR = "navigation_bar_show_new";
 
-         /**
-         * @hide
-         */
-        public static final String QS_TILE_STYLE = "qs_tile_style";
-
         /**
          * Battery style
          * @hide
@@ -5788,7 +5783,6 @@ public static final String THEMING_SETTINGS_DASHBOARD_ICONS =
             PRIVATE_SETTINGS.add(BUTTON_BACKLIGHT_TIMEOUT);
             PRIVATE_SETTINGS.add(BUTTON_BRIGHTNESS);
             PRIVATE_SETTINGS.add(FORCE_SHOW_NAVBAR);
-            PRIVATE_SETTINGS.add(QS_TILE_STYLE);
 	    PRIVATE_SETTINGS.add(VIBRATE_ON_CONNECT);
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4140,9 +4140,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.FORCE_SHOW_NAVBAR),
                     false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                     Settings.System.QS_TILE_STYLE),
-                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4166,7 +4163,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             setOldMobileType();
             setLockScreenMediaBlurLevel();
             updateChargingAnimation();
-            updateTileStyle();
             updateNavigationBar(false);
         }
     }
@@ -4213,11 +4209,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         mShowNavBar = showNavBar;
     }
-
-     public void updateTileStyle() {
-         int qsTileStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
-                 Settings.System.QS_TILE_STYLE, 0, mLockscreenUserManager.getCurrentUserId());
-     }
 
     public int getWakefulnessState() {
         return mWakefulnessLifecycle.getWakefulness();
