14f33797bc772a292d70d2d30c89961601bd83b4
Ritzz <riteshm321@gmail.com>
2021-02-13 08:34:57 +0100

    [SQUASH] Revert old Gaming Mode
    - Also revert headsup notifications to inline with gaming mode
    
    Revert "[WIP]SystemUI: GamingModeTile: Disable hw keys & navbar"
    
    This reverts commit deae4beedbfb295833106231d125ea41a6683b42.
    
    Revert "base: Call gaming mode tile gaming macro"
    
    This reverts commit 901137b499bfd35c77b8ce0c522c8beb31c4b4ec.
    
    Revert "base: Make gaming mode QS tile save and load the state of settings"
    
    This reverts commit a6eb8ad36ef0422227879719bb7218448d0a9155.
    
    Revert "base: Add gaming mode QS tile settings"
    
    This reverts commit d2979a8d096d0033d27772ba9a14a46ddda02052.
    
    Revert "SystemUI: GamingModeTile: Improve code"
    
    This reverts commit f6d6073ee983b5d52f2bd11520382570d5bb404c.
    
    Revert "SystemUI: GamingModeTile: Show "What's This?" on long click"
    
    This reverts commit 481207d9336822f3332132a81d688eacaa932bfe.
    
    Revert "SystemUI: Add Gaming mode tile"
    
    This reverts commit df4ea1689cc5f08b0fd63f70d7a54e8aef2076e1.
    
    Revert " fixup! Less boring heads up option"
    
    This reverts commit e0e21103f8b4e57dadbfe6db2727616aeaed64ea.
    
    Revert "Less boring heads up option [1/2]"
    
    This reverts commit 29f9777d5fabdb525b87bfe2612fff66061103eb.
    
    Change-Id: Ifca79d25ce0022434540e488c900519bb147ef37

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -42,7 +42,6 @@ import com.android.systemui.qs.tiles.DataSwitchTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HeadphonesBuddyTile;
-import com.android.systemui.qs.tiles.GamingModeTile;
 import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.ImmersiveTile;
@@ -116,7 +115,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SyncTile> mSyncTileProvider;
     private final Provider<AODTile> mAODTileProvider;
     private final Provider<ThemeTile> mThemeTileProvider;
-    private final Provider<GamingModeTile> mGamingModeTileProvider;
     private final Provider<VolumeTile> mVolumeTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
@@ -161,7 +159,6 @@ public class QSFactoryImpl implements QSFactory {
             Provider<SyncTile> syncTileProvider,
             Provider<AODTile> aodTileProvider,
             Provider<ThemeTile> themeTileProvider,
-            Provider<GamingModeTile> gamingModeTileProvider,
             Provider<VolumeTile> volumeTileProvider) {
         mQsHostLazy = qsHostLazy;
         mWifiTileProvider = wifiTileProvider;
@@ -202,7 +199,6 @@ public class QSFactoryImpl implements QSFactory {
         mSyncTileProvider = syncTileProvider;
         mAODTileProvider = aodTileProvider;
         mThemeTileProvider = themeTileProvider;
-        mGamingModeTileProvider = gamingModeTileProvider;
         mVolumeTileProvider = volumeTileProvider;
     }
 
@@ -291,8 +287,6 @@ public class QSFactoryImpl implements QSFactory {
                 return mAODTileProvider.get();
             case "theme":
                 return mThemeTileProvider.get();
-           case "gaming":
-                return mGamingModeTileProvider.get();
             case "volume_panel":
                 return mVolumeTileProvider.get();
         }
