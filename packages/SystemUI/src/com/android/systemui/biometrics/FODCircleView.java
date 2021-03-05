b99204829c8470acbefbe218eeb9b634ce356191
Dil3mm4 <dil3mm4.dev@gmail.com>
2021-03-05 23:04:37 +0900

    FODCircleView: various improvements
    Handled more visibility cases
    Tests executed:
        - Added a Bluetooth device as Trusted Device via Smart Lock, ensured FOD wasn't visible
        - Used other biometric methods to unlock the device, ensured FOD wasn't visible
        - Enabled On-body detection, ensured FOD wasn't visible
        - Enabled Trusted places, ensured FOD wasn't visible
        - Triggered biometric failure with too many unrecognized attempts, ensured FOD wasn't visible while transitioning back and forth from AOD.
        - Triggered floating biometric dialog and ensured its functionality
    
    LuK1337:
    Solved a visibility edge case
    Tests executed:
        - On a secured lockscreen, with "double tap power button to open camera gesture" disabled, double tap the power button,
          ensure FOD correct visibility
    
    Stallix:
    Allow Screen off FOD to continue working if device is Trusted
    Tests executed:
        - Turn off Screen off FOD, ensured FOD wasn't visible
        - Turn on Screen off FOD, ensured FOD unlocked even while device is Trusted
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -83,10 +83,11 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
     private int mColorBackground;
     private int mDreamingOffsetY;
 
+    private boolean mIsBiometricRunning;
     private boolean mIsBouncer;
+    private boolean mIsCircleShowing;
     private boolean mIsDreaming;
     private boolean mIsKeyguard;
-    private boolean mIsCircleShowing;
 
     private boolean mDozeEnabled;
     private boolean mFodGestureEnable;
@@ -178,6 +179,24 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
 
     private KeyguardUpdateMonitorCallback mMonitorCallback = new KeyguardUpdateMonitorCallback() {
         @Override
+        public void onBiometricAuthenticated(int userId, BiometricSourceType biometricSourceType,
+                boolean isStrongBiometric) {
+            // We assume that if biometricSourceType matches Fingerprint it will be
+            // handled here, so we hide only when other biometric types authenticate
+            if (biometricSourceType != BiometricSourceType.FINGERPRINT) {
+                hide();
+            }
+        }
+
+        @Override
+        public void onBiometricRunningStateChanged(boolean running,
+                BiometricSourceType biometricSourceType) {
+            if (biometricSourceType == BiometricSourceType.FINGERPRINT) {
+                mIsBiometricRunning = running;
+            }
+        }
+
+        @Override
         public void onDreamingStateChanged(boolean dreaming) {
             mIsDreaming = dreaming;
             updateAlpha();
@@ -225,6 +244,14 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
                 hide();
             } else {
                 hideCircle();
+            }
+        }
+
+        @Override
+        public void onStartedWakingUp() {
+            if (!mScreenTurnedOn &&
+                    mUpdateMonitor.isFingerprintDetectionRunning()) {
+                show();
             }
         }
 
@@ -517,10 +544,20 @@ public class FODCircleView extends ImageView implements TunerService.Tunable {
             return;
         }
 
+        if (mIsKeyguard && mUpdateMonitor.getUserCanSkipBouncer(mUpdateMonitor.getCurrentUser()) &&
+                !mFodGestureEnable) {
+            // Ignore show calls if user can skip bouncer
+            return;
+        }
+
+        if (mIsKeyguard && !mIsBiometricRunning) {
+            return;
+        }
+
         updatePosition();
 
-        dispatchShow();
         setVisibility(View.VISIBLE);
+        dispatchShow();
     }
 
     public void hide() {
