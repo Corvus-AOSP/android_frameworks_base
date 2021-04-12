135aa6c533a8a30cc2bb8e70fd0f9c72a2bdb552
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-04-12 19:27:43 +0530

    Fixup! applock: Check current and called activity package before locking
    - If some app launches an activity with FLAG_ACTIVITY_NEW_TASK while being in foreground, it will be locked again.
      Fix it by comparing the calling package and package name of the activity to be launched, only lock if they are different.
    
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>
    Change-Id: I7715586f84fa66562b1d0ad67d0e354b811d8c80

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -1015,7 +1015,7 @@ class ActivityStarter {
         final String pkg = aInfo == null ? null : aInfo.packageName;
         if (mService.isAppLocked(pkg) && !mService.isAppOpened(pkg)
                 && !mService.isAlarmOrCallIntent(intent)
-                && !getTopStackPackageName().equals(pkg)) {
+                && !callingPackage.equals(pkg)) {
             if (DEBUG_APPLOCK) Slog.d(TAG_APPLOCK, "Locked pkg:" + pkg + " intent:" + intent);
             mService.mAppLockService.setAppIntent(pkg, intent);
             mService.mAppLockService.launchBeforeActivity(pkg);
