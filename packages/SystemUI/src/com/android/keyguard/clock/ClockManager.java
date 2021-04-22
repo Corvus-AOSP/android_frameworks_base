0b4407f2293e5caa8dbd3ec0c67acb23cde6cc98
Rahul <rahul731999@gmail.com>
2021-04-22 21:53:20 +0530

    Introduce Octavi's lockscreen clock
    Change-Id: I63dc817a8412a8d6989167eab08ed26dbbe17afd

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -177,7 +177,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new StickerTagClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new ShapeShiftClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SquaryClockController(res, layoutInflater, colorExtractor));
-
+        addBuiltinClock(() -> new OctaviDigitalClockController(res, layoutInflater, colorExtractor));
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
         mWidth = dm.widthPixels;
--- packages/SystemUI/src/com/android/keyguard/clock/SquaryClockController.java
+++ packages/SystemUI/src/com/android/keyguard/clock/SquaryClockController.java
@@ -146,7 +146,7 @@ public class SquaryClockController implements ClockPlugin {
 
     @Override
     public View getView() {
-        return null
+        return null;
     }
 
     @Override
