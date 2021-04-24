cee69bdf39ba5cb41836923126b894fd3f30d1e5
Laker <lakerman1987@gmail.com>
2021-04-24 21:33:38 +0300

    base: Notifications background opacity [1/2]
    neobuddy89: Adapt to 0-255 range like we have for others.
    
    Corvus:
    Not gonna use opacity sliders for Power Menu as such, Lets remove that code and keep it only for notifications
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If35bf56932329e8afcbdd5d9db164d119b2a4fd3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5979,6 +5979,13 @@ public final class Settings {
         public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
 
         /**
+         * Whether to change the transparency of the notifications
+         * @hide
+         */
+        public static final String NOTIFICATION_BG_ALPHA = "notification_bg_alpha";
+
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
