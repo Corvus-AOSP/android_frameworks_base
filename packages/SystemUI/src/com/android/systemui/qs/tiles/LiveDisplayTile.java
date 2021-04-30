6b12a980ad85c2f57048570d607cd50c58d7fe36
StarkDroid <gamerprince.exp@gmail.com>
2021-05-01 01:50:03 +0530

    Revert "LiveDisplayTile: Enable for outdoor mode and skip night display on HWC2"
    This reverts commit e922d67b1965ea04c4bf6e26a412f758c990be9c.

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
@@ -20,7 +20,6 @@ package com.android.systemui.qs.tiles;
 import static com.android.internal.custom.hardware.LiveDisplayManager.FEATURE_MANAGED_OUTDOOR_MODE;
 import static com.android.internal.custom.hardware.LiveDisplayManager.MODE_AUTO;
 import static com.android.internal.custom.hardware.LiveDisplayManager.MODE_DAY;
-import static com.android.internal.custom.hardware.LiveDisplayManager.MODE_NIGHT;
 import static com.android.internal.custom.hardware.LiveDisplayManager.MODE_OFF;
 import static com.android.internal.custom.hardware.LiveDisplayManager.MODE_OUTDOOR;
 
@@ -31,9 +30,9 @@ import android.database.ContentObserver;
 import android.hardware.display.ColorDisplayManager;
 import android.os.Handler;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.service.quicksettings.Tile;
 
-import com.android.internal.app.ColorDisplayController;
 import com.android.internal.util.ArrayUtils;
 import com.android.systemui.plugins.qs.QSTile.LiveDisplayState;
 import com.android.systemui.qs.QSHost;
@@ -65,7 +64,6 @@ public class LiveDisplayTile extends QSTileImpl<LiveDisplayState> {
 
     private int mDayTemperature;
 
-    private final boolean mNightDisplayAvailable;
     private final boolean mOutdoorModeAvailable;
 
     private final LiveDisplayManager mLiveDisplay;
@@ -75,7 +73,6 @@ public class LiveDisplayTile extends QSTileImpl<LiveDisplayState> {
     @Inject
     public LiveDisplayTile(QSHost host) {
         super(host);
-        mNightDisplayAvailable = ColorDisplayController.isAvailable(mContext);
 
         Resources res = mContext.getResources();
         TypedArray typedArray = res.obtainTypedArray(R.array.live_display_drawables);
@@ -112,7 +109,7 @@ public class LiveDisplayTile extends QSTileImpl<LiveDisplayState> {
 
     @Override
     public boolean isAvailable() {
-        return !mNightDisplayAvailable || mOutdoorModeAvailable;
+        return !ColorDisplayManager.isNightDisplayAvailable(mContext);
     }
 
     @Override
@@ -190,12 +187,10 @@ public class LiveDisplayTile extends QSTileImpl<LiveDisplayState> {
 
         while (true) {
             nextMode = Integer.valueOf(mValues[next]);
-            // Skip outdoor mode if it's unsupported, skip the day setting
-            // if it's the same as the off setting, and skip night display
-            // on HWC2
+            // Skip outdoor mode if it's unsupported, and skip the day setting
+            // if it's the same as the off setting
             if ((!mOutdoorModeAvailable && nextMode == MODE_OUTDOOR) ||
-                    (mDayTemperature == OFF_TEMPERATURE && nextMode == MODE_DAY) ||
-                    (mNightDisplayAvailable && (nextMode == MODE_DAY || nextMode == MODE_NIGHT))) {
+                    (mDayTemperature == OFF_TEMPERATURE && nextMode == MODE_DAY)) {
                 next++;
                 if (next >= mValues.length) {
                     next = 0;
