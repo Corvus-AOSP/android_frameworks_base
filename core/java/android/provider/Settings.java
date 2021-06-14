627741d2b0321e8b629da723bb2ed1048742c5b4
StarkDroid <gamerprince.exp@gmail.com>
2021-06-14 16:44:17 +0200

    QS Kill button: Remove dependency for toggle
    Its not really a view obstruction of any sort, Dont really wanna spam our settings with 69 toggles for nothing
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Change-Id: I87d81ae34b1018c289440bba30cce3429673a930

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5374,14 +5374,6 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
         
         /**
-         * Whether to show the kill app button in notification guts
-         *
-         * @hide
-         */
-        public static final String NOTIFICATION_GUTS_KILL_APP_BUTTON =
-                "notification_guts_kill_app_button";
-
-        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
