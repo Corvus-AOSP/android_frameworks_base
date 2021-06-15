2beffd1cc6e32cc60d93754420415fda412d877e
jhenrique09 <jhsv09@gmail.com>
2021-06-15 20:57:40 -0300

    AppLock: Don't trigger if starting new intent of current running package
    Change-Id: I26d1fe4e37d2c86a8de12714634c1da0745ad0da

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -1014,7 +1014,8 @@ class ActivityStarter {
 
         final String pkg = aInfo == null ? null : aInfo.packageName;
         if (mService.isAppLocked(pkg) && !mService.isAppOpened(pkg)
-                && !mService.isAlarmOrCallIntent(intent)) {
+                && !mService.isAlarmOrCallIntent(intent)
+                && !getTopStackPackageName().equals(pkg)) {
             if (DEBUG_APPLOCK) Slog.d(TAG_APPLOCK, "Locked pkg:" + pkg + " intent:" + intent);
             mService.mAppLockService.setAppIntent(pkg, intent);
             mService.mAppLockService.launchBeforeActivity(pkg);
