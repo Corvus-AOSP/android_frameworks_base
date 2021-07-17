a60a646d5bbb6f354aa487e535b8ca150f4b0cc6
Trishiraj <gamerprince.exp@gmail.com>
2021-07-17 23:52:32 +0200

    Fix QS Tint color to play well with QS Tile styles
    Change-Id: I16a4fa75da6473ea0a12742a179dde7973cef8eb

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -549,10 +549,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
             case Tile.STATE_INACTIVE:
 		return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
             case Tile.STATE_ACTIVE:
-		if (setQsUseNewTint)
-                   return Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
-		else
-		   return Utils.getColorAttrDefaultColor(context, android.R.attr.colorPrimary);
+		return Utils.getColorAttrDefaultColor(context, android.R.attr.textColorPrimary);
             default:
                 Log.e("QSTile", "Invalid state " + state);
                 return 0;
