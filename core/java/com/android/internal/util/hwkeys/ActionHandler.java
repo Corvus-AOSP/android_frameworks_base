ffb4ee3ceadea32946e7462862195a89afc15223
Trishiraj <gamerprince.exp@gmail.com>
2021-07-30 22:27:15 +0200

    Revert "ActionHandler: Set proximity check to false for toggleCameraFlash"
    This reverts commit c4e5627e95dce89451e7935c9a56ee6f8c45495a.

--- core/java/com/android/internal/util/hwkeys/ActionHandler.java
+++ core/java/com/android/internal/util/hwkeys/ActionHandler.java
@@ -339,7 +339,7 @@ public class ActionHandler {
         private static void toggleFlashlight() {
             IStatusBarService service = getStatusBarService();
             try {
-                service.toggleCameraFlash(false);
+                service.toggleCameraFlash();
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
