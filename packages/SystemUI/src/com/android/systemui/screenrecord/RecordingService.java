8633c701b3240f6ede3a4a652f27930fde39245f
Trishiraj <gamerprince.exp@gmail.com>
2021-09-26 13:54:09 +0530

    Screenrecord: Bring back extended features
    This was lost while adding the GamingMode
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Change-Id: If99d44f357d84fb9c1f1fff0797a95c76e599fe8

--- packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/RecordingService.java
@@ -156,7 +156,11 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
         UserHandle currentUser = new UserHandle(mCurrentUserId);
         switch (action) {
             case ACTION_START:
-                doStartRecording(intent.getIntExtra(EXTRA_AUDIO_SOURCE, 0), intent.getBooleanExtra(EXTRA_SHOW_TAPS, false));
+                doStartRecording(intent.getIntExtra(EXTRA_AUDIO_SOURCE, 0),
+                 intent.getBooleanExtra(EXTRA_SHOW_TAPS, false), 
+                 intent.getBooleanExtra(EXTRA_SHOW_STOP_DOT, false),
+                 intent.getBooleanExtra(EXTRA_LOW_QUALITY, false),
+                 intent.getBooleanExtra(EXTRA_LONGER_DURATION, false));
                 break;
 
             case ACTION_STOP_NOTIF:
@@ -217,18 +221,24 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
         return Service.START_STICKY;
     }
 
-    protected void doStartRecording(int audioSource, boolean showTaps) {
+    protected void doStartRecording(int audioSource, boolean showTaps, boolean showStopDot, boolean lowQuality, boolean longerDuration) {
         int mCurrentUserId = mUserContextTracker.getCurrentUserContext().getUserId();
         mAudioSource = ScreenRecordingAudioSource
                 .values()[audioSource];
         Log.d(TAG, "recording with audio source" + mAudioSource);
         mShowTaps = showTaps;
+        mShowStopDot = showStopDot;
+        mLowQuality = lowQuality;
+        mLongerDuration = longerDuration;
 
         mOriginalShowTaps = Settings.System.getInt(
                 getApplicationContext().getContentResolver(),
                 Settings.System.SHOW_TOUCHES, 0) != 0;
 
         setTapsVisible(mShowTaps);
+        setStopDotVisible(mShowStopDot);
+        setLowQuality(mLowQuality);
+        setLongerDuration(mLongerDuration);
 
         mRecorder = new ScreenMediaRecorder(
                 mUserContextTracker.getCurrentUserContext(),
@@ -564,8 +574,8 @@ public class RecordingService extends Service implements MediaRecorder.OnInfoLis
         private ArrayList<IRecordingCallback> mCallbackList = new ArrayList<>();
 
         @Override
-        public void startRecording(int audioSource, boolean showTaps) throws RemoteException {
-            doStartRecording(audioSource, showTaps);
+        public void startRecording(int audioSource, boolean showTaps, boolean showStopDot, boolean lowQuality, boolean longerDuration) throws RemoteException {
+            doStartRecording(audioSource, showTaps, showStopDot, lowQuality, longerDuration);
         }
 
         @Override
