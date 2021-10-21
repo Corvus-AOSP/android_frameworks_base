11d0105b25aefd6f29439b0fa5167a6c82bc9ffb
ezio84 <brabus84@gmail.com>
2021-10-21 18:07:40 +0000

    Allow doubletap/longpress power to toggle torch [1/2]
    AEX Edits:
    - Port to S
    - handle camera gesture here instead of extensions
    - simplify code
    
    Thanks to beanstown106 for the initial longpress action calls in
    PhoneWindowManager (improved by lineage guys)
    
    If the user enables double tap for camera action, torch action will
    automatically switch to long press mode.
    If the user enables double tap for torch, double tap for camera
    will be disabled accordingly.
    
    Remove the settings pref is the device doesn't have flashlight
    
    A 170ms delay for double tap action is not annoying on single tap
    to wake up the device.
    
    Vibrate when toggling torch to let the user know of unwanted toggling
    in the pocket (even if vibration is disabled by user)
    
    Allow torch action also on ambient display
    
    mydongistiny: Fixed for android10
    Change-Id: I12da044f86c7b625872607529cf8524615cf576b
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5320,6 +5320,16 @@ public final class Settings {
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
 
         /**
+         * Whether the torch launch gesture to double tap or long press the power button when the
+         * screen is off should be enabled. *
+         * 0: disabled
+         * 1: double tap power for torch
+         * 2: long tap power for torch
+         * @hide
+         */
+        public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
