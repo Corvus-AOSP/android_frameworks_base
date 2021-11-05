f310f11d966680a1ca58e5d6f9add7672478323e
tysonwei <tysonwei@realtek.com>
2021-11-05 18:02:28 +0800

    Fix potential race condition problem
    If we call forceSuspend to enter STR and then resume from it,
    we should make sure that mForceSuspendActive is set to false
    to wake up device correctly.
    
    Bug: 205257123
    Test: 1. forceSuspend
          2. wait until kernel enter suspend mode
          3. press power button to wakeUp
    
    Change-Id: I85c9fd2aaed602cc927b11d256b88965c95ae8d5
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -4194,7 +4194,6 @@ public final class PowerManagerService extends SystemService
             return success;
         }
     }
-
     /**
      * Low-level function turn the device off immediately, without trying
      * to be clean.  Most people should use {@link ShutdownThread} for a clean shutdown.
