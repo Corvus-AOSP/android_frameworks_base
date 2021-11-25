58207b38052f3e0b4671b4cab87336e9a497b09b
jhonboy121 <alfredmathew05@gmail.com>
2021-11-25 17:32:41 +0530

    base: allow hiding navbar [1/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I0782380947a5357f82ecbb3bbefb1a12866e1ae4

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -85,5 +85,6 @@ public class SystemSettings {
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
         Settings.System.OMNI_ADVANCED_REBOOT,
+        Settings.System.FULLSCREEN_GESTURES,
     };
 }
