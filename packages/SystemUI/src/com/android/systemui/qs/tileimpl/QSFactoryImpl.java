22f33bc89e928e578c8cf647e6d50c30bc31dc3c
Rasenkai <rasenkai99@gmail.com>
2021-12-14 14:48:31 +0000

    base: IS_DEBUGGABLE -> IS_ENG
    * based on commit 4e750b88ad3ef24e33248de09bbddbfac517423b
    -----
    git grep -l 'Build.IS_DEBUGGABLE' | xargs sed -i 's/Build.IS_DEBUGGABLE/Build.IS_ENG/g'
    
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>
    Change-Id: I97e5a13b5019a0c5eeb2c514a7a914782297510a

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -339,11 +339,11 @@ public class QSFactoryImpl implements QSFactory {
         }
 
         // Debug tiles.
-        /*if (Build.IS_DEBUGGABLE) {
+        if (Build.IS_ENG) {
             if (tileSpec.equals(GarbageMonitor.MemoryTile.TILE_SPEC)) {
                 return mMemoryTileProvider.get();
             }
-        }*/
+        }
 
         // Broken tiles.
         Log.w(TAG, "No stock tile spec: " + tileSpec);
