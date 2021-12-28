d35de08cfa5cc006ccfa5ebb516cc172521ada6e
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial corvus SystemUI stub
    Change-Id: I67f36f20cc01dfc82572f2241287c3e739592306
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ packages/SystemUI/src/com/corvus/systemui/dagger/CorvusSysUIComponent.java
@@ -0,0 +1,25 @@
+package com.corvus.systemui.dagger;
+
+import com.android.systemui.dagger.DefaultComponentBinder;
+import com.android.systemui.dagger.DependencyProvider;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.dagger.SystemUIBinder;
+import com.android.systemui.dagger.SysUIComponent;
+import com.android.systemui.dagger.SystemUIModule;
+
+import dagger.Subcomponent;
+
+@SysUISingleton
+@Subcomponent(modules = {
+        DefaultComponentBinder.class,
+        DependencyProvider.class,
+        SystemUIBinder.class,
+        SystemUIModule.class,
+        CorvusSystemUIModule.class})
+public interface CorvusSysUIComponent extends SysUIComponent {
+    @SysUISingleton
+    @Subcomponent.Builder
+    interface Builder extends SysUIComponent.Builder {
+        CorvusSysUIComponent build();
+    }
+}
