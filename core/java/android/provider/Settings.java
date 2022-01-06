f5489cfe08f2b71c8b1712292593dc837a5ca532
Ido Ben-Hur <idoybh2@gmail.com>
2022-01-06 20:45:48 +0200

    BatteryMeterView: Allow disabling QS battery estimates [1/2]
    Adapted to Sv2
    
    Change-Id: I4cf66725509606361f18809a040a9d1c3f2663ac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5418,6 +5418,12 @@ public final class Settings {
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
         /**
+         * Whether to show battery estimates in QS
+         * @hide
+         */
+        public static final String QS_SHOW_BATTERY_ESTIMATE = "qs_show_battery_estimate";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
