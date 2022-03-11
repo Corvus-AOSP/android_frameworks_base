ff2f8564370fb13ab5e351f0a7912596e4abfffc
Trishiraj <gamerprince.exp@gmail.com>
2022-03-11 14:42:50 -0600

    Fix derp in Corvusutils

--- core/java/com/android/internal/util/corvus/CorvusUtils.java
+++ core/java/com/android/internal/util/corvus/CorvusUtils.java
@@ -74,7 +74,7 @@ public class CorvusUtils {
     public static final String INTENT_SCREENSHOT = "action_take_screenshot";
     public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
-    private static final String TAG = "AEXUtils";
+    private static final String TAG = "CorvusUtils";
 
     private static final boolean DEBUG = false;
 
