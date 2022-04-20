efa93c9040187538db4c9d87177e263231d30ddf
StarkDroid <gamerprince.exp@gmail.com>
2022-04-20 07:47:19 +0000

    VolumePanel: Design Changes [2/2]
    - Added an animated cog wheel to show expand/collapse volume panel animation
    - Long press on the cog wheel now takes us to sound settings
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Change-Id: Ibc56bff25af5b69bd005bf56bd8860d4c37ac956

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -105,6 +105,9 @@ import android.view.accessibility.AccessibilityManager;
 import android.view.View.OnLongClickListener;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.LinearInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -132,7 +135,6 @@ import com.android.systemui.plugins.VolumeDialog;
 import com.android.systemui.plugins.VolumeDialogController;
 import com.android.systemui.plugins.VolumeDialogController.State;
 import com.android.systemui.plugins.VolumeDialogController.StreamState;
-import com.android.systemui.statusbar.phone.ExpandableIndicator;
 import com.android.systemui.statusbar.policy.AccessibilityManagerWrapper;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
@@ -248,9 +250,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     private ViewGroup mODICaptionsView;
     private CaptionsToggleImageButton mODICaptionsIcon;
     private View mSettingsView;
+    private RotateAnimation rotateAnimation;
     private ImageButton mSettingsIcon;
+    private ImageButton mExpandRows;
     private View mExpandRowsView;
-    private ExpandableIndicator mExpandRows;
     private FrameLayout mZenIcon;
     private final List<VolumeRow> mRows = new ArrayList<>();
     private ConfigurableTexts mConfigurableTexts;
@@ -641,10 +644,10 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
         mSettingsView = mDialog.findViewById(R.id.settings_container);
         mSettingsIcon = mDialog.findViewById(R.id.settings);
-        mSettingsIcon.setOnLongClickListener(this);
 
         mExpandRowsView = mDialog.findViewById(R.id.expandable_indicator_container);
         mExpandRows = mDialog.findViewById(R.id.expandable_indicator);
+        mExpandRows.setOnLongClickListener(this);
 
         mRoundedBorderBottom = mDialog.findViewById(R.id.rounded_border_bottom);
 
@@ -674,8 +677,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
             setGravity(mODICaptionsView, Gravity.LEFT);
             setLayoutGravity(mODICaptionsView, Gravity.LEFT);
-
-            mExpandRows.setRotation(-90);
         }
 
         if (mRows.isEmpty()) {
@@ -1282,7 +1283,11 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             mExpandRows.setOnClickListener(v -> {
                 mExpanded = !mExpanded;
                 updateRowsH(mDefaultRow, true);
-                mExpandRows.setExpanded(mExpanded);
+                if (!mExpanded) {
+                    rotateIcon();
+                } else {
+                    rotateIconReverse();
+                }
             });
         }
     }
@@ -1625,9 +1630,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                     mDialog.dismiss();
                     tryToRemoveCaptionsTooltip();
                     mExpanded = false;
-                    if (mExpandRows != null) {
-                        mExpandRows.setExpanded(mExpanded);
-                    }
                     mAnimatingRows = 0;
                     mDefaultRow = null;
                     mIsAnimatingDismiss = false;
@@ -2666,7 +2668,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     }
 
     public boolean onLongClick(View v) {
-        if (v == mSettingsIcon) {
+        if (v == mExpandRows) {
             startSoundActivity();
         }
         return false;
@@ -2679,6 +2681,22 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             "com.android.settings.Settings$SoundSettingsActivity");
         mMediaOutputDialogFactory.dismiss();
         mActivityStarter.startActivity(nIntent, true /* dismissShade */);
+    }
+
+    private void rotateIcon() {
+        rotateAnimation = new RotateAnimation(0, 180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(400);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        mExpandRows.startAnimation(rotateAnimation);
+    }
+
+    private void rotateIconReverse() {
+        rotateAnimation = new RotateAnimation(180, 0, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(400);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        mExpandRows.startAnimation(rotateAnimation);
     }
 
     private final class VolumeSeekBarChangeListener implements OnSeekBarChangeListener {
