98e01d1990a03c2b52b5a2b119051ef200a47cb3
StarkDroid <gamerprince.exp@gmail.com>
2022-06-17 16:35:20 -0500

    Revert "SystemUI: Invoke keyguard exit animation"
    This reverts commit fa2c7194cb4cb59da043c31dc8e219853d914827.

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2598,7 +2598,6 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
             if (callback != null) {
                 if (mWakeAndUnlocking) {
                     mDrawnCallback = callback;
-                    notifyDrawn(callback);
                 } else {
                     notifyDrawn(callback);
                 }
