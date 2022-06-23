2421d7e9d995014c023aa5ed786ba0de50e1731f
StarkDroid <gamerprince.exp@gmail.com>
2022-06-23 13:33:13 -0500

    Add QS Panel Styles [1/3]
    Note for other roms:
    This is the jankiest possible implementation that DOES WORK but is more of an extra work.
    Ideally i'd like to have this implemented within in-line ThemeOverLayApplier and ThemeOverlayController but rather
    its being handled through settings with a custom observer at the moment to trigger the overlays.
    
    So why this impl?
    I was just curious to test this out, but once i saw that its implemented and is working, i decided to push it and work on the refinement later
    
    Feel free to do the refinement on your end if you are able to :D
    
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5923,6 +5923,11 @@ public final class Settings {
         public static final String QS_TRANSPARENCY = "qs_transparency";
 
         /**
+         * @hide
+         */
+        public static final String QS_PANEL_STYLE = "qs_panel_style";
+
+        /**
          * Force full screen for devices with cutout
          * @hide
          */
