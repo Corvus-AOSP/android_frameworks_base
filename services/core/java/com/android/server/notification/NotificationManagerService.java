0259e60d322dd5f9c80c4a23ae0ebd692732eb5b
Daniel Norman <danielnorman@google.com>
2022-08-12 11:40:41 -0700

    Do not send AccessibilityEvent if notification is for different user.
    Bug: 237540408
    Test: BuzzBeepBlinkTest#testA11yCrossUserEventNotSent
    Change-Id: I62a875e26e214847ec72ce3c41b4f2fa8e597e07
    (cherry picked from commit a367c0a16a9070ed6bee3028ac5bbc967773ee8f)
    Merged-In: I62a875e26e214847ec72ce3c41b4f2fa8e597e07

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -7041,7 +7041,8 @@ public class NotificationManagerService extends SystemService {
                 && (record.getSuppressedVisualEffects() & SUPPRESSED_EFFECT_STATUS_BAR) != 0;
         if (!record.isUpdate
                 && record.getImportance() > IMPORTANCE_MIN
-                && !suppressedByDnd) {
+                && !suppressedByDnd
+                && isNotificationForCurrentUser(record)) {
             sendAccessibilityEvent(notification, record.getSbn().getPackageName());
             sentAccessibilityEvent = true;
         }
