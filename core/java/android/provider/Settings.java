ef7e871b04de5c435a29795eaab639d723a14a07
Trishiraj <gamerprince.exp@gmail.com>
2022-09-07 20:01:40 +0000

    Simplify FP auth vibration
    Too many toggles too much mess, lets unify them instead
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6020,16 +6020,10 @@ public final class Settings {
 
 
         /**
-         * Whether to enable or disable vibration on succesful fingerprint auth
+         * Whether to enable or disable vibration on fingerprint auth
          * @hide
          */
-        public static final String FP_SUCCESS_VIBRATE = "fingerprint_success_vib";
-
-        /**
-         * Whether to enable or disable vibration on failed fingerprint auth
-         * @hide
-         */
-        public static final String FP_ERROR_VIBRATE = "fingerprint_error_vib";
+        public static final String FP_AUTH_VIBRATE = "fingerprint_vibration";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
