55cbfe4c9cb70ce0e9bbe685370380f4447c632a
Alexander Winkowski <dereference23@outlook.com>
2022-10-14 23:25:32 +0200

    PixelPropsUtils: Spoof Nexus 6P for GMS unstable process
    To fix Play Integrity, taken from https://github.com/kdrag0n/safetynet-fix/pull/207
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/corvus/PixelPropsUtils.java
+++ core/java/com/android/internal/util/corvus/PixelPropsUtils.java
@@ -251,6 +251,8 @@ public class PixelPropsUtils {
             if (packageName.equals(PACKAGE_GMS) &&
                     processName.equals(PACKAGE_GMS + ".unstable")) {
                 sIsGms = true;
+                setPropValue("FINGERPRINT", "google/angler/angler:6.0/MDB08L/2343525:user/release-keys");
+                setPropValue("MODEL", "angler");
             }
             if (isPixelDevice) {
                 if (packageName.equals(PACKAGE_GMS) &&
