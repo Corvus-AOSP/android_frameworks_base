09a95b2f39faa8c5faffac75d039812183a9036f
Pranav Vashi <neobuddy89@gmail.com>
2022-10-16 00:31:02 +0530

    PixelPropsUtils: Update pixel devices list
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/corvus/PixelPropsUtils.java
+++ core/java/com/android/internal/util/corvus/PixelPropsUtils.java
@@ -149,10 +149,13 @@ public class PixelPropsUtils {
 
     // Codenames for currently supported Pixels by Google
     private static final String[] pixelCodenames = {
+            "cheetah",
+            "panther",
+            "bluejay",
             "oriole",
             "raven",
-            "redfin",
             "barbet",
+            "redfin",
             "bramble",
             "sunfish",
             "coral",
@@ -226,11 +229,12 @@ public class PixelPropsUtils {
                     if (isPixelDevice) return;
                     propsToChange.putAll(propsToChangePixel5);
                 }
+            } else if (isPixelDevice) {
+                return;
             } else if (packageName.equals("com.android.vending")) {
                 sIsFinsky = true;
                 return;
             } else {
-                if (isPixelDevice) return;
                 if ((Arrays.asList(packagesToChangePixel7Pro).contains(packageName))
                         || Arrays.asList(extraPackagesToChange).contains(packageName)) {
                     propsToChange.putAll(propsToChangePixel7Pro);
