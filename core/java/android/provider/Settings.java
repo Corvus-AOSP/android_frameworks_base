3a99e4c32b368d2bfd745e2f7826a22413fe99cf
Trishiraj <gamerprince.exp@gmail.com>
2022-10-29 22:17:10 +0000

    Add taskbar to Settings provider
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Change-Id: I0e23ef586e1013f82f3a2784bea798d67b9e6cf8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5652,6 +5652,11 @@ public final class Settings {
         public static final String ROAMING_INDICATOR_ICON = "roaming_indicator_icon";
 
         /**
+         * @hide
+         */
+        public static final String ENABLE_TASKBAR = "enable_taskbar";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
