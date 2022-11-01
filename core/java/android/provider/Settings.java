abb7bb50dcad9294fe1dcb88fd4fffdb4b67a79d
minaripenguin37 <alexfinhart@gmail.com>
2022-11-01 19:40:11 +0800

    base: Make IME space optional
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>
    Co-authored-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    Signed-off-by: hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
    Change-Id: Icc273e99efd6645d78b08b908e4f35608377d036

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5701,6 +5701,12 @@ public final class Settings {
         public static final String DATA_DISABLED_ICON = "data_disabled_icon";
 
         /**
+         * Whether to show IME space
+         * @hide
+         */
+        public static final String HIDE_IME_SPACE_ENABLE = "hide_ime_space_enable";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
