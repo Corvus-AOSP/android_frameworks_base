164cd75665eb3ccef485210ade1f0dfdbfd856b8
rdkng1 <richardraya39@gmail.com>
2023-08-26 22:05:49 +0530

    PixelPropUtils: Add GamesProps   Based on PixelPropsUtils, GamesProps will spoof the device needed to unlock the FPS of the following games:   * Free Fire - Spoof Asus ROG Phone 1 will unlock 90 FPS   * COD Mobile - Spoof Xperia 5 || will unlock 120 FPS (only on multiplayer mode)   * PUBG Mobile - Spoof OnePlus 8 Pro will unlock 90 FPS   * Wild Rift - Spoof OnePlus 8 Pro will unlock 120 FPS   * Cyber Hunter - Spoof OnePlus 8 Pro will unlock 90 FPS   * Fortnite - Spoof OnePlus 8 Pro will unlock 90 FPS
    @neobuddy89: Adapt GamesProps to existing PixelProps and update games list.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/corvus/PixelPropsUtils.java
+++ core/java/com/android/internal/util/corvus/PixelPropsUtils.java
@@ -66,6 +66,38 @@ public class PixelPropsUtils {
         "com.google.android.googlequicksearchbox"
     };
 
+    private static final Map<String, Object> propsToChangeROG1;
+    private static final String[] packagesToChangeROG1 = {
+            "com.dts.freefireth",
+            "com.dts.freefiremax",
+            "com.madfingergames.legends"
+    };
+
+    private static final Map<String, Object> propsToChangeXP5;
+    private static final String[] packagesToChangeXP5 = {
+            "com.activision.callofduty.shooter",
+            "com.tencent.tmgp.kr.codm",
+            "com.garena.game.codm",
+            "com.vng.codmvn"
+    };
+
+    private static final Map<String, Object> propsToChangeOP8P;
+    private static final String[] packagesToChangeOP8P = {
+            "com.tencent.ig",
+            "com.pubg.imobile",
+            "com.pubg.krmobile",
+            "com.pubg.newstate",
+            "com.vng.pubgmobile",
+            "com.rekoo.pubgm",
+            "com.tencent.tmgp.pubgmhd",
+            "com.riotgames.league.wildrift",
+            "com.riotgames.league.wildrifttw",
+            "com.riotgames.league.wildriftvn",
+            "com.netease.lztgglobal",
+            "com.epicgames.fortnite",
+            "com.epicgames.portal"
+    };
+
     static {
         propsToChange = new HashMap<>();
         propsToChange.put("BRAND", "google");
@@ -88,6 +120,14 @@ public class PixelPropsUtils {
         propsToChangePixel3XL.put("PRODUCT", "crosshatch");
         propsToChangePixel3XL.put("MODEL", "Pixel 3 XL");
         propsToChangePixel3XL.put("FINGERPRINT", "google/crosshatch/crosshatch:11/RQ3A.210605.005/7349499:user/release-keys");
+    propsToChangeROG1 = new HashMap<>();
+        propsToChangeROG1.put("MODEL", "ASUS_Z01QD");
+        propsToChangeROG1.put("MANUFACTURER", "asus");
+    propsToChangeXP5 = new HashMap<>();
+        propsToChangeXP5.put("MODEL", "SO-52A");
+    propsToChangeOP8P = new HashMap<>();
+        propsToChangeOP8P.put("MODEL", "IN2020");
+        propsToChangeOP8P.put("MANUFACTURER", "OnePlus");        
     }
 
     public static void setProps(String packageName) {
@@ -125,10 +165,33 @@ public class PixelPropsUtils {
             }
         }
         // Set proper indexing fingerprint
-        if (packageName.equals("com.google.android.settings.intelligence")) {
+    if (packageName.equals("com.google.android.settings.intelligence")) {
             setPropValue("FINGERPRINT", Build.DATE);
         }
-    }
+            } else {
+                if (Arrays.asList(packagesToChangeROG1).contains(packageName)) {
+                    if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
+                    for (Map.Entry<String, Object> prop : propsToChangeROG1.entrySet()) {
+                        String key = prop.getKey();
+                        Object value = prop.getValue();
+                        setPropValue(key, value);
+                    }
+            } else if (Arrays.asList(packagesToChangeXP5).contains(packageName)) {
+                if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
+                    for (Map.Entry<String, Object> prop : propsToChangeXP5.entrySet()) {
+                        String key = prop.getKey();
+                        Object value = prop.getValue();
+                        setPropValue(key, value);
+                }
+            } else if (Arrays.asList(packagesToChangeOP8P).contains(packageName)) {
+                if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
+                    for (Map.Entry<String, Object> prop : propsToChangeOP8P.entrySet()) {
+                        String key = prop.getKey();
+                        Object value = prop.getValue();
+                        setPropValue(key, value);
+                    }
+            }        
+        }
 
     private static void setPropValue(String key, Object value) {
         try {
@@ -143,4 +206,5 @@ public class PixelPropsUtils {
             Log.e(TAG, "Failed to set prop " + key, e);
         }
     }
+
 }
